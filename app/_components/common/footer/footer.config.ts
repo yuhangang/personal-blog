b415b2f2b3a2138798f02f74735ead208d253cc0
Yu Hang Ang <redrainhang@gmail.com>
2024-12-11 10:27:36 +0800

    initial implementation of GA and updated footer

--- app/_components/common/footer/footer.config.ts
+++ app/_components/common/footer/footer.config.ts
@@ -1,24 +1,41 @@
-const socialLinks = [
+type SocialLink = {
+  name: string;
+  icon: string;
+  iconSize: number;
+  url: string;
+};
+
+const socialLinks: SocialLink[] = [
   {
     name: "GitHub",
     icon: "{ }",
+    iconSize: 16,
     url: "https://github.com/yuhangang",
   },
   {
     name: "Instagram",
     icon: "◇",
+    iconSize: 24,
     url: "https://instagram.com/yuhangang",
   },
   {
     name: "X",
     icon: "×",
+    iconSize: 24,
     url: "https://x.com/redrainhang",
   },
   {
     name: "LinkedIn",
     icon: "IN",
+    iconSize: 16,
     url: "https://linkedin.com/in/yu-hang-ang-b8510010b/",
+  },
+  {
+    name: "Email",
+    icon: "✉︎",
+    iconSize: 24,
+    url: "mailto:redrainhang@gmail.com",
   },
 ];
 
-export { socialLinks };
+export { socialLinks, type SocialLink };
