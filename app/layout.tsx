2fdb9c1f14640ad7a84fa0a3ca0589f0c524cde1
Yu Hang Ang <redrainhang@gmail.com>
2024-12-11 12:33:34 +0800

    handle null value of proess env

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,17 @@ import ClientLayout from "@/lib/client-layout";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  if (!process.env.NEXT_PUBLIC_API_URL) {
+    throw new Error("Missing NEXT_PUBLIC_API_URL in environment variables");
+  }
+
   return (
     <html lang="en">
       <body>
         <StyledComponentsRegistry>
           <ClientLayout>{children}</ClientLayout>
         </StyledComponentsRegistry>
-        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID ?? ""} />
       </body>
     </html>
   );
