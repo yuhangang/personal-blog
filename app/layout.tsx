5ce77bd247e0a71423ecb132af8a5e11ab9cdc48
Yu Hang Ang <redrainhang@gmail.com>
2024-12-11 12:22:10 +0800

    Fix GA env configuration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,15 +4,13 @@ import ClientLayout from "@/lib/client-layout";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  console.log("GA_ID", process.env.GA_ID);
-
   return (
     <html lang="en">
       <body>
         <StyledComponentsRegistry>
           <ClientLayout>{children}</ClientLayout>
         </StyledComponentsRegistry>
-        <GoogleAnalytics gaId={process.env.GA_ID} />
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
       </body>
     </html>
   );
